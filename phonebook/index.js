910d54e71aa914209003453f67d7285c76d01886
baveryk <io.kainama@gmail.com>
2026-08-06 20:22:41 +0700

    Finished exercise 3.7 and 3.8

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', (req) => {
+    if (req.method === 'POST' && req.body) {
+        return JSON.stringify(req.body)
+    }
+
+    return ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
