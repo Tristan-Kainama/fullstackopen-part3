c0ae6a02456a596821961e36c354fa72ff189738
baveryk <io.kainama@gmail.com>
2026-08-14 15:47:55 +0700

    Finished Exercise 3.20

--- phonebook/models/person.js
+++ phonebook/models/person.js
@@ -22,7 +22,13 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function(value) {
+                return /^\d{2,3}-\d+$/.test(value) && value.length >= 8;
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
     }  
 })
 
